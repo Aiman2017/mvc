8a2f0d6533cc39962899ce074489175d56f6e550
Aiman2017 <50306902+Aiman2017@users.noreply.github.com>
2023-02-28 02:54:00 +0300

    Update Home.php

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -8,15 +8,19 @@ class Home
         $arr['name'] = 'Aiman';
         $arr['family'] = 'Al-Raidii';
 
-            foreach ($this->findAll() as  $key => $value) {
-                if ($value['name'] === $arr['name'] && $value['family'] === $arr['family'] ) {
-                  return'this user is in our database';
+       if (empty($this->findAll())) {
+           $this->insert($arr);
+       }else {
+           foreach ($this->findAll() as  $key => $value) {
+               if ($value['name'] === $arr['name'] && $value['family'] === $arr['family'] ) {
+                   return'this user is in our database';
 
-                }else {
-                    $this->insert($arr);
-                    break;
-                }
-        }
+               }else {
+                   $this->insert($arr);
+                   break;
+               }
+           }
+       }
 
         $data['title'] = 'MVC';
         $this->view('home', $data);
